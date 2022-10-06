43b30de1626ccbf3c2c4d854231c41b3e3a0b0c8
Nildo-Maciel <erivanildomaciel@gmail.com>
2022-10-06 20:19:47 -0300

    toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/Vector.svg';
 import { BASE_URL } from '../../utils/request';
 
@@ -11,7 +12,7 @@ type props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("secesso");
+        toast.info("SMS enviado com sucesso!");
     });    
 }
 
